5a5b855494d2e84ffa2ebe328e56f8141f115318
Artem Vasenin <vasart169@gmail.com>
2024-12-16 01:11:22 +0000

    feat(rust): add day 1 part2 and day 2 both parts

--- 2024/rust/src/main.rs
+++ 2024/rust/src/main.rs
@@ -1,11 +1,16 @@
+use std::collections::{HashMap, HashSet};
 use std::fs;
 use std::io;
 use std::iter::zip;
 use std::path::Path;
 
+fn read_file(data_dir: &Path, day: i32) -> String {
+    let file_path = data_dir.join(format!("day{}.txt", day));
+    fs::read_to_string(file_path).expect("Can't read the file")
+}
+
 fn day1(data_dir: &Path) {
-    let file_path = data_dir.join("day1.txt");
-    let contents = fs::read_to_string(file_path).expect("Can't read the file");
+    let contents = read_file(data_dir, 1);
 
     let (mut first_list, mut second_list): (Vec<i32>, Vec<i32>) = contents
         .lines()
@@ -20,11 +25,85 @@ fn day1(data_dir: &Path) {
     first_list.sort();
     second_list.sort();
 
-    let sum = zip(first_list, second_list)
-        .map(|(left, right)| (left - right).abs())
-        .reduce(|total, next| total + next)
-        .unwrap();
+    let sum: i32 = zip(&first_list, &second_list)
+        .map(|(left, right)| (*left - *right).abs())
+        .sum();
     println!("Day 1: {}", sum);
+
+    let mut counts: HashMap<i32, i32> = HashMap::new();
+    for i in second_list {
+        *counts.entry(i).or_default() += 1;
+    }
+
+    let similarity: i32 = first_list
+        .into_iter()
+        .collect::<HashSet<_>>()
+        .into_iter()
+        .map(|v| v * *counts.entry(v).or_default())
+        .sum();
+
+    println!("Day 1, part2: {}", similarity);
+}
+
+fn day2(data_dir: &Path) {
+    let contents = read_file(data_dir, 2);
+
+    let reports: Vec<Vec<i32>> = contents
+        .lines()
+        .map(|line| {
+            line.split(' ')
+                .map(|chars| chars.parse::<i32>().unwrap())
+                .collect()
+        })
+        .collect();
+
+    fn check_report(report: &Vec<i32>) -> bool {
+        let mut increasing = report.clone();
+        increasing.sort();
+        let mut decreasing = report.clone();
+        decreasing.sort();
+        decreasing.reverse();
+
+        if *report != increasing && *report != decreasing {
+            return false;
+        }
+
+        zip(&increasing[..increasing.len() - 1], &increasing[1..])
+            .map(|(smaller, larger)| {
+                let diff = *larger - *smaller;
+                diff > 0 && diff < 4
+            })
+            .all(|x| x)
+    }
+
+    let total_valid = reports
+        .clone()
+        .into_iter()
+        .map(|report| check_report(&report))
+        .filter(|x| *x)
+        .count();
+    println!("Day 2: {}", total_valid);
+
+    // Reports are fairly short and I can't be bothered to re-write the checking logic properly
+    let valid_with_dampner = reports
+        .into_iter()
+        .map(|report| {
+            if check_report(&report) {
+                return true;
+            }
+            for idx in 0..report.len() {
+                let mut candidate = report.clone();
+                candidate.remove(idx);
+                if check_report(&candidate) {
+                    return true;
+                }
+            }
+            false
+        })
+        .filter(|x| *x)
+        .count();
+
+    println!("Day2, part2: {}", valid_with_dampner);
 }
 
 fn main() {
@@ -38,4 +117,5 @@ fn main() {
 
     let data_dir = Path::new(input.trim());
     day1(data_dir);
+    day2(data_dir);
 }
